bb8fa6f228e67f1e3d441c035b43957231b60a8b
Joe Curtis <joecurtis9999@gmail.com>
2022-03-20 09:27:11 +0000

    created employee table

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,7 +6,7 @@ CREATE DATABASE employee_info_db;
 -- use database
 USE employee_info_db
 
--- create table
+-- create department table
 CREATE TABLE department (
 
 id INT NOT NULL AUTO_INCREMENT,
@@ -14,4 +14,34 @@ id INT NOT NULL AUTO_INCREMENT,
 name VARCHAR(30) NOT NULL,
 
 PRIMARY KEY(id)
+);
+
+-- create role table
+CREATE TABLE role (
+
+id INT NOT NULL AUTO_INCREMENT,
+
+title VARCHAR(30) NOT NULL,
+
+salary DECIMAL(10,2) NOT NULL,
+
+department_id INT NOT NULL,
+
+PRIMARY KEY (id)
+);
+
+-- create employee table
+CREATE TABLE employee (
+
+id INT NOT NULL AUTO_INCREMENT,
+
+first_name VARCHAR(30) NOT NULL,
+
+last_name VARCHAR(30) NOT NULL,
+
+role_id INT NOT NULL,
+
+manager_id INT,
+
+PRIMARY KEY (id)
 );
